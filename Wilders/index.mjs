1c7a44b36f775279399d7a69036ebcbd1b9f7096
HugoBDN <hugo.bedouin@gmail.com>
2023-11-21 17:06:11 +0100

    voici mon workshop node npm

---
+++ Wilders/index.mjs
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+// const chalk = require("chalk");
+
+function sayHello(){
+    console.log(chalk.green("je m'appelle Abir"));
+    console.log(chalk.red("je m'appelle Hugo"));
+    console.log(chalk.blue("je m'appelle Sylvain"));
+}
+sayHello();
